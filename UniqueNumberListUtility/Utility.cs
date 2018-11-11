077b93e058350d7286484034322d535b7f8959cc
Unknown <wewantthefunk@live.com>
2018-11-11 09:29:20 -0500

    initial

---
+++ UniqueNumberListUtility/Utility.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueNumberListUtility
+{
+    public class Utility
+    {
+        SortedList<int, int> _numberCount = new SortedList<int, int>();
+
+        public int[] GetUnique (List<int[]> list)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int[] array in list)
+            {
+                for (int x = 0; x < array.Length; x++)
+                {
+                    if (_numberCount.ContainsKey(array[x]))
+                    {
+                        _numberCount[array[x]]++;
+                    }
+                    else
+                    {
+                        _numberCount.Add(array[x], 1);
+                    }
+                }
+            }
+
+            foreach(int number in _numberCount.Keys)
+            {
+                if (_numberCount[number] == 1)
+                {
+                    result.Add(number);
+                }
+            }
+
+            return result.ToArray();
+        }
+    }
+}
---
+++ UniqueNumberLists/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UniqueNumberListUtility;
+
+namespace UniqueNumberLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int[]> lists = new List<int[]>();
+
+            int[] one = new int[] { 1, 2, 3 };
+
+            int[] two = new int[] { 2, 3, 4};
+
+            int[] three = new int[] { 3, 5, 6 };
+
+            lists.Add(one);
+            lists.Add(two);
+            lists.Add(three);
+
+            Utility utility = new Utility();
+
+            int[] result = utility.GetUnique(lists);
+
+            foreach(int i in result)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
